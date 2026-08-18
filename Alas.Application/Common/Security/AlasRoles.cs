18d9156c6076ef231fc94ed4a12a8e39a2119b86
jcmcjms <mrjamestabilog@gmail.com>
2026-08-18 21:04:44 +0800

    feat: add AlasRoles.cs connection of roles and specific permissions

---
+++ Alas.Application/Common/Security/AlasRoles.cs
@@ -0,0 +1,26 @@
+﻿namespace Alas.Application.Common.Security;
+
+public static class AlasRoles
+{
+    public const string Admin = "Admin";
+    public const string Auditor = "Auditor";
+    public const string Approver = "Approver";
+
+    public static readonly Dictionary<string, string[]> Matrix = new()
+    {
+        [Admin] = AlasPermissions.All,
+
+        [Auditor] =
+        [
+            AlasPermissions.LoansRead,
+            AlasPermissions.ReportsRead,
+            AlasPermissions.AuditRead
+        ],
+
+        [Approver] =
+        [
+            AlasPermissions.LoansRead,
+            AlasPermissions.LoansApprove
+        ]
+    };
+}
