3d21908e1a1235e3dc34d14df6cf86ad94b8b9b7
jcmcjms <mrjamestabilog@gmail.com>
2026-08-18 20:59:30 +0800

    feat: add permissions

--- Alas.Application/Common/Security/AlasPermissions.cs
+++ Alas.Application/Common/Security/AlasPermissions.cs
@@ -2,5 +2,26 @@
 
 public static class AlasPermissions
 {
+    public const string LoansRead = "loans.read";
+    public const string LoansWrite = "loans.write";
+    public const string LoansApprove =  "loans.approve";
+    public const string LoansDisburse = "loans.disburse";
     
+    public const string ReportsRead = "reports.read";
+    public const string AuditRead = "audits.read";
+    
+    public const string UsersManage =  "users.manage";
+    public const string RolesManage = "roles.manage";
+
+    public static readonly string[] All =
+    [
+        LoansRead,
+        LoansWrite,
+        LoansApprove,
+        LoansDisburse,
+        ReportsRead,
+        AuditRead,
+        UsersManage,
+        RolesManage
+    ];
 }
