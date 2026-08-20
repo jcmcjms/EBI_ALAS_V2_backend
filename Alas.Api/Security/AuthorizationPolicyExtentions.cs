c70591a51704ea97a633a7ed3e25690451da638c
jcmcjms <mrjamestabilog@gmail.com>
2026-08-20 20:55:45 +0800

    feat: add Permissions Providers

---
+++ Alas.Api/Security/AuthorizationPolicyExtentions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Alas.Api.Security;
+
+public static class AuthorizationPolicyExtentions
+{
+    public static AuthorizationPolicyBuilder RequirePermission(this AuthorizationPolicyBuilder builder,
+        string permission)
+    {
+        return builder.AddRequirements(new PermissionRequirement(permission));
+    }
+}
---
+++ Alas.Api/Security/PermissionAuthorizationHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Alas.Application.Common.Security;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Alas.Api.Security;
+
+public sealed class PermissionAuthorizationHandler: AuthorizationHandler<PermissionRequirement>
+{
+    private readonly IUserPermissionProvider _permissionProvider;
+
+    public PermissionAuthorizationHandler(IUserPermissionProvider permissionProvider)
+    {
+        _permissionProvider = permissionProvider;
+    }
+
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+    {
+        var userIdClaim = context.User.FindFirstValue(ClaimTypes.NameIdentifier) ??
+                          context.User.FindFirstValue("sub");
+
+        if (!Guid.TryParse(userIdClaim, out var userId))
+        {
+            return;
+        }
+        
+       
+        
+    }
+}
