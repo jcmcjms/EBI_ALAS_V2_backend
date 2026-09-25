cfb0f9cc1661f04e243d51e3aa03bb16259f6e21
jcmcjms <mrjamestabilog@gmail.com>
2026-09-25 16:49:50 +0800

    chore(db): add routing escalation columns migration

---
+++ EBI.ALAS.Api/Migrations/20260925084914_AddRoutingEscalationColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EBI.ALAS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoutingEscalationColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MatchedButUnstaffedTier",
+                table: "LoanApplications",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NoAuthorityReason",
+                table: "LoanApplications",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MatchedButUnstaffedTier",
+                table: "LoanApplications");
+
+            migrationBuilder.DropColumn(
+                name: "NoAuthorityReason",
+                table: "LoanApplications");
+        }
+    }
+}
