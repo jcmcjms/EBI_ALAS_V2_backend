f4f30574a8d4e47b3c8342a26ac5e41e296cf13a
jcmcjms <mrjamestabilog@gmail.com>
2026-09-25 14:26:57 +0800

    chore(db): add pending EF Core migration

---
+++ EBI.ALAS.Api/Migrations/20260925061246_SyncPendingModelChanges.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EBI.ALAS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncPendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LoanApplications_Users_DocumentsFlaggedById",
+                table: "LoanApplications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LoanApplications_Users_DocumentsFlaggedById",
+                table: "LoanApplications",
+                column: "DocumentsFlaggedById",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LoanApplications_Users_DocumentsFlaggedById",
+                table: "LoanApplications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LoanApplications_Users_DocumentsFlaggedById",
+                table: "LoanApplications",
+                column: "DocumentsFlaggedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
